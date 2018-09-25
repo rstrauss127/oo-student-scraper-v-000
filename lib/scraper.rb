047b8d1cdc8c5963fc0e67b0c231e91bf79ede35
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 02:35:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,6 +29,9 @@ students
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    links = doc.css(".social-icon-container a")['href']
 
   end
 
