432bff81dfb54a7ccdcdab1ee3806022cf95e4bc
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 00:46:11 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,24 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+
+    roster =  doc.css("student-card")
+  
+
+    binding.pry
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
